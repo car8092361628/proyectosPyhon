e223a7b92532e7bef061c53b81456943a0bfde1c
carlos_garciaseminariotecnologia <44681324+car8092361628@users.noreply.github.com>
2024-06-05 19:30:57 -0400

    Add files via upload

---
+++ tare2.py
@@ -0,0 +1,14 @@
+num1=float(input("ingrese el primer valor"))
+num2=float(input("ingrese el segudo valor"))
+num3=float(input("ingrese el tercer valor"))
+
+promedio=num1+num2+num3/3
+
+print("El promedio es:", promedio)
+
+
+num4=float(input("ingrese el cuarto valor"))
+num5=float(input("ingrese el quinto valor"))
+
+suma=num4+num5
+print("la suma es", suma)
